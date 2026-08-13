1c2b1ff45be7d9f2899a16c8cad250804292ee1f
administrator <administrator@iispl>
2026-08-13 15:32:54 +0530

    Implemented daily cheque report functionality

--- src/com/iispl/reporting/JasperReportRunner.java
+++ src/com/iispl/reporting/JasperReportRunner.java
@@ -1,11 +1,36 @@
 package com.iispl.reporting;
 
-import java.io.File;
+import java.sql.Connection;
 
 import net.sf.jasperreports.engine.JasperCompileManager;
+import net.sf.jasperreports.engine.JasperExportManager;
+import net.sf.jasperreports.engine.JasperFillManager;
+import net.sf.jasperreports.engine.JasperPrint;
 import net.sf.jasperreports.engine.JasperReport;
 
 public class JasperReportRunner {
+	public void generateDailyChequeReport() {
+		Connection connection = null;
+		try {
+			connection = DBConnection.getConnection();
+			
+			JasperReport jasperReport = JasperCompileManager.compileReport("reports/daily_cheque_report.jrxml");
+			
+			JasperPrint jasperPrint = JasperFillManager.fillReport(jasperReport,null,connection);
+			
+			JasperExportManager.exportReportToPdfFile(jasperPrint, "output/daily_cheque_report.pdf");
 
-	
+            System.out.println("Daily cheque report generated successfully.");
+ } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+        	if (connection != null) {
+                try {
+                    connection.close();
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+	}
 }
