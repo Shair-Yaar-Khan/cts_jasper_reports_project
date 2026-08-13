e11ace8ef95f9beb3503bbfc2a0fd200ee2150dc
SHAIR YAAR KHAN <shairyaark932@gmail.com>
2026-08-13 15:35:11 +0530

    bank summary and batch summary

--- src/com/iispl/reporting/JasperReportRunner.java
+++ src/com/iispl/reporting/JasperReportRunner.java
@@ -1,11 +1,42 @@
 package com.iispl.reporting;
 
-import java.io.File;
-
+import java.sql.Connection;
 import net.sf.jasperreports.engine.JasperCompileManager;
+import net.sf.jasperreports.engine.JasperExportManager;
+import net.sf.jasperreports.engine.JasperFillManager;
+import net.sf.jasperreports.engine.JasperPrint;
 import net.sf.jasperreports.engine.JasperReport;
 
 public class JasperReportRunner {
 
+	public void generateBatchSummaryReport() throws Exception {
+
+	    String inputFile = "reports/batch_summary_report.jrxml";
+
+	    JasperReport jasperReport = JasperCompileManager.compileReport(inputFile);
+
+	    Connection connection = DBConnection.getConnection();
+
+	    JasperPrint jasperPrint = JasperFillManager.fillReport(jasperReport,null,connection);
+
+	    JasperExportManager.exportReportToPdfFile(jasperPrint,"output/batch_summary_report.pdf");
+
+	    System.out.println("Batch Summary Report generated successfully.");
+	}
 	
+	public static void generateBankSummaryReport() throws Exception {
+
+	    String inputFile = "reports/bank_summary_report.jrxml";
+
+	    JasperReport jasperReport = JasperCompileManager.compileReport(inputFile);
+
+	    Connection connection = DBConnection.getConnection();
+
+	    JasperPrint jasperPrint = JasperFillManager.fillReport(jasperReport,null,connection);
+
+	    JasperExportManager.exportReportToPdfFile(jasperPrint,"output/bank_summary_report.pdf");
+
+	    System.out.println("Bank Summary Report generated successfully.");
+	    
+	}
 }
