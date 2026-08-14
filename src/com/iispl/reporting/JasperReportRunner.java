aa4566ab8716c20c2374692436e8b6728be78c6a
iispl <iispl@iispl>
2026-08-14 10:18:40 +0530

    solved merge issue

--- src/com/iispl/reporting/JasperReportRunner.java
+++ src/com/iispl/reporting/JasperReportRunner.java
@@ -17,9 +17,131 @@ import net.sf.jasperreports.engine.JasperFillManager;
 import net.sf.jasperreports.engine.JasperPrint;
 import net.sf.jasperreports.engine.JasperReport;
 
-public class JasperReportRunner {
 
+public class JasperReportRunner {
+	public void generateHighValueChequeReport() throws Exception {
+
+        JasperReport report = JasperCompileManager.compileReport(
+                "reports/high_value_report.jrxml"
+        );
+
+        Map<String, Object> parameters = new HashMap<>();
+
+        Connection connection = DBConnection.getConnection();
+
+        JasperPrint print = JasperFillManager.fillReport(
+                report,
+                parameters,
+                connection
+        );
+
+        JasperExportManager.exportReportToPdfFile(
+                print,
+                "output/high_value_report.pdf"
+        );
+        
+        System.out.println("High Value Cheque Report generated successfully.");
+
+        connection.close();
+    }
+    public void micrRepairReport() throws SQLException, JRException
+    {
+    	
+
+            Connection connection =
+                    DBConnection.getConnection();
+
+            System.out.println("Database connected successfully.");
+
+            String jrxmlFile =
+                    "reports/micr_repair_report.jrxml";
+
+            JasperReport jasperReport =
+                    JasperCompileManager.compileReport(jrxmlFile);
+
+            System.out.println("JRXML compiled successfully.");
+
+
+         
+
+            JasperPrint jasperPrint =
+                    JasperFillManager.fillReport(
+                            jasperReport,
+                            null,
+                            connection
+                    );
+
+            System.out.println("Report filled successfully.");
+
+
+            String outputFile =
+                    "output/micr_repair_report.pdf";
+
+            JasperExportManager.exportReportToPdfFile(
+                    jasperPrint,
+                    outputFile
+            );
+
+            System.out.println(
+                    "PDF generated successfully:"
+            );
+
+            System.out.println(outputFile);
+
+    }
+
+
+	public void generateDailyChequeReport() {
+		Connection connection = null;
+		
+			try {
+				connection = DBConnection.getConnection();
+				
+				JasperReport jasperReport = JasperCompileManager.compileReport("reports/daily_cheque_report.jrxml");
+				
+				JasperPrint jasperPrint = JasperFillManager.fillReport(jasperReport,null,connection);
+				
+				JasperExportManager.exportReportToPdfFile(jasperPrint, "output/daily_cheque_report.pdf");
+			} catch (SQLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}catch(JRException e) {
+				e.printStackTrace();
+			}
+	        System.out.println("Daily Cheque Report generated successfully.");
+
+			
+  }
+	public void generateBatchSummaryReport() throws Exception {
+
+	    String inputFile = "reports/batch_summary_report.jrxml";
+
+	    JasperReport jasperReport = JasperCompileManager.compileReport(inputFile);
+
+	    Connection connection = DBConnection.getConnection();
+
+	    JasperPrint jasperPrint = JasperFillManager.fillReport(jasperReport,null,connection);
+
+	    JasperExportManager.exportReportToPdfFile(jasperPrint,"output/batch_summary_report.pdf");
+
+	    System.out.println("Batch Summary Report generated successfully.");
+	}
 	
+	public static void generateBankSummaryReport() throws Exception {
+
+	    String inputFile = "reports/bank_summary_report.jrxml";
+
+	    JasperReport jasperReport = JasperCompileManager.compileReport(inputFile);
+
+	    Connection connection = DBConnection.getConnection();
+
+	    JasperPrint jasperPrint = JasperFillManager.fillReport(jasperReport,null,connection);
+
+	    JasperExportManager.exportReportToPdfFile(jasperPrint,"output/bank_summary_report.pdf");
+
+	    System.out.println("Bank Summary Report generated successfully.");
+	    
+	}
 	
 	public void generateRejectedChequeReport() throws Exception{
 
@@ -67,6 +189,5 @@ public class JasperReportRunner {
             System.out.println(e.getMessage());
         }
     }
-	
-	
 }
+
